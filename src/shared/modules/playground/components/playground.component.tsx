618167f38b42e6da5adc9135ac8fb6c4392facc3
Ahmed-Mabrook <ahmed@webbio.nl>
2019-09-18 09:50:33 +0200

    feat: added icon component

--- src/shared/modules/playground/components/playground.component.tsx
+++ src/shared/modules/playground/components/playground.component.tsx
@@ -3,15 +3,13 @@ import React from "react";
 import { PlaygroundContainerProps } from "@app/modules/playground";
 import IconDefault from "@assets/icons/chevron-down.svg";
 import style from "./playground-component.module.scss";
-import { LinkComponent } from "@app/core/link";
 import { IconComponent } from "@app/core/icon";
-import { NavBarComponent } from "@app/modules/nav-bar";
 
 export interface IPlaygroundComponentProps {}
 
 const PlaygroundComponent = (props: IPlaygroundComponentProps & PlaygroundContainerProps) => (
   <div className={"uk-container"}>
-    <NavBarComponent />
+    <IconComponent icon={IconDefault} size="24px" />
   </div>
 );
 
