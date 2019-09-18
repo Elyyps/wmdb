381b9f847e9f0e4ad0dff1272162f7d440bdf941
Ahmed-Mabrook <ahmed@webbio.nl>
2019-09-18 11:54:25 +0200

    feat: added image component

--- src/shared/modules/playground/components/playground.component.tsx
+++ src/shared/modules/playground/components/playground.component.tsx
@@ -4,12 +4,13 @@ import { PlaygroundContainerProps } from "@app/modules/playground";
 import IconDefault from "@assets/icons/chevron-down.svg";
 import style from "./playground-component.module.scss";
 import { IconComponent } from "@app/core/icon";
+import { ImageComponent } from "@app/core/image";
 
 export interface IPlaygroundComponentProps {}
 
 const PlaygroundComponent = (props: IPlaygroundComponentProps & PlaygroundContainerProps) => (
   <div className={"uk-container"}>
-    <IconComponent icon={IconDefault} size="24px" />
+    <ImageComponent src={IconDefault} errorImage={IconDefault} />
   </div>
 );
 
