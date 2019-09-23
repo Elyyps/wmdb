a569cab755e59fd6f07e206cde0390513ef02606
Pedro Knup <phknup@gmail.com>
2019-09-23 10:13:33 +0200

    feat: added see more panel

--- src/shared/core/arrow-panel-list/arrow-panel-list.component.tsx
+++ src/shared/core/arrow-panel-list/arrow-panel-list.component.tsx
@@ -1,16 +1,50 @@
-
 import * as React from "react";
 import styles from "./arrow-panel-list-component.module.scss";
+import { IconComponent } from "../icon";
+import { ArrowPanel } from "../arrow-panel/arrow-panel.component";
+import { SeeMorePanel } from "../see-more-panel";
+
 
 export interface IArrowPanelListComponentProps {
-
+  backgroundColor?: string;
+  icon?: string;
+  items?: any[];
+  paddingBottom?: string;
+  paddingTop?: string;
+  pattern?: boolean;
+  title: string;
 }
 
 const ArrowPanelListComponent = (props: IArrowPanelListComponentProps) => {
-  return ( <div className={styles["arrow-panel-list"]}>
+  const { items } = props;
 
-  </div>)
+  return (
+    <div className={styles["arrow-panel-list"]}>
+      {items && (
+        <div className="faq-overview__posts uk-grid uk-child-width-1-2@s">
+          {items.map((row, key) => (
+            <div key={key}>
+              <div className="faq-overview__head">
+                {row.icon && <IconComponent icon={row.icon} size="34px" />}
+
+                <h3>{row.title}</h3>
+              </div>
+              {row.items.map((item: any, i: number) => (
+                <div key={i} className="faq-overview__item">
+                  <ArrowPanel {...item} />
+                </div>
+              ))}
+
+              {row.linkMore && (
+                <div className="faq-overview__item">
+                  <SeeMorePanel {...row.linkMore} />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
-
 export { ArrowPanelListComponent };
-  
--- src/shared/modules/playground/components/playground.component.tsx
+++ src/shared/modules/playground/components/playground.component.tsx
@@ -3,6 +3,8 @@ import { PlaygroundContainerProps } from "@app/modules/playground";
 
 import style from "./playground-component.module.scss";
 import { ArrowPanel } from "@app/core/arrow-panel/arrow-panel.component";
+import { ArrowPanelListComponent } from "@app/core/arrow-panel-list";
+import ETEN from "@assets/icons/eten.svg";
 
 export interface IPlaygroundComponentProps {}
 
@@ -10,9 +12,46 @@ const PlaygroundComponent = (props: IPlaygroundComponentProps & PlaygroundContai
   <div>
     <div style={{ backgroundColor: "#e8f2fc" }} className={style["main"]}>
       Playground!!
-      <div >
-        <ArrowPanel title="Title #1" link="/" />
-        <ArrowPanel title="Title #2" link="/" />
+      <div>
+        <ArrowPanelListComponent
+          title="Arrow Panel List title"
+          items={[
+            {
+              icon: ETEN,
+              title: "Spel & Ontspanning",
+              items: [
+                {
+                  title: "Wat is Lorem Ipsum?",
+                  link: "/"
+                },
+                {
+                  title: "Hoe lang zijn mijn aanbiedingen of vacatures zichtbaar op indebuurt?",
+                  link: "/"
+                },
+                {
+                  title: "Hoe kan ik mijn (wekelijkse) aanbieding of vacature aanleveren?",
+                  link: "/"
+                },
+                {
+                  title: "Hoe lang staat mijn artikel op de homepagina, waar komt het vandaan?",
+                  link: "/"
+                },
+                {
+                  title: "Waar kan ik het vinden?",
+                  link: "/"
+                },
+                {
+                  title: "Waar kan ik het vinden?",
+                  link: "/"
+                }
+              ],
+              linkMore: {
+                title: "Alle veelgestelde vragen over musea",
+                link: "/"
+              }
+            }
+          ]}
+        />
       </div>
     </div>
   </div>
