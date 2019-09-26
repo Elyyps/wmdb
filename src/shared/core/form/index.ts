69679c1a8ba00c7d29a19461eff2f16af6023de7
Pedro Knup <phknup@gmail.com>
2019-09-26 09:47:34 +0200

    feat: implemented component

--- src/shared/core/form/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./input.component";
-export * from "./textarea.component";
-export * from "./checkbox.component";
-export * from "./radio-button.component";
-export * from "./select.component";
