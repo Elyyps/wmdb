730caf80aa878371282ecb5cb1ee8f6f90972c97
Ahmed-Mabrook <ahmed@webbio.nl>
2019-09-27 09:16:39 +0200

    fix: changed library for header gallery

--- src/shared/modules/header-gallery/header-gallery.component.tsx
+++ src/shared/modules/header-gallery/header-gallery.component.tsx
@@ -4,8 +4,9 @@ import { IconComponent } from "@app/core/icon";
 import { ImageComponent } from "@app/core/image";
 import IconImage from "@assets/icons/images.svg";
 import IconPlay from "@assets/icons/play.svg";
-import Slider, { Settings } from "react-slick";
-import { BasicArrow } from "./basic-arrow.component";
+
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 export interface IHeaderGalleryComponentProps {
   images: string[];
@@ -14,11 +15,11 @@ export interface IHeaderGalleryComponentProps {
 const HeaderGalleryComponent = (props: IHeaderGalleryComponentProps) => {
   const [currentImage, setCurrentImage] = React.useState(0);
   const smallImagesStart = 2;
-  const handleChange = (prev: number, next: number) => {
-    if (next === 1) {
-      setCurrentImage(currentImage + 1);
-    } else {
-      setCurrentImage(currentImage - 1);
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
     }
   };
 
@@ -27,42 +28,26 @@ const HeaderGalleryComponent = (props: IHeaderGalleryComponentProps) => {
     currentImage < 0 && setCurrentImage(props.images.length - 1);
   }, [currentImage]);
 
-  const ref: any = React.createRef();
-  ref.current = "slick";
-
-  const settings: Settings = {
-    dots: false,
-    arrows: true,
-    infinite: true,
-    prevArrow: <BasicArrow prev={true} />,
-    nextArrow: <BasicArrow />,
-    responsive: [
-      {
-        breakpoint: 1199,
-        settings: {
-          arrows: false,
-          dots: true,
-          slidesToScroll: 1,
-          slidesToShow: 1
-        }
-      }
-    ],
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    speed: 500
-  };
-
   return (
     <div className={styles["header-gallery"]}>
       <div className="uk-container">
         <div className={styles["header-gallery__container"]}>
           <div className={`${styles["header-gallery__col"]} ${styles["large"]}`}>
-            <Slider ref={ref} {...settings} beforeChange={handleChange}>
-              {props.images.slice(currentImage, props.images.length).map((item: string, key: number) => (
+            {/* <GallerySliderComponent currentIndex={currentImage} images={props.images} /> */}
+
+            <AliceCarousel
+              onSlideChanged={e => setCurrentImage(e.slide)}
+              dotsDisabled={true}
+              startIndex={currentImage}
+              responsive={responsive}
+              infinite={true}
+            >
+              {props.images.map((item: string, key: number) => (
                 <ImageComponent src={item} key={key} />
               ))}
-            </Slider>
-            <div className={` ${styles["header-gallery__info"]} ${"uk-visible@l"} `}>
+            </AliceCarousel>
+
+            <div className={`${styles["header-gallery__info"]} ${"uk-visible@l"}`}>
               <div className={styles["slider-info"]}>
                 <div className={styles["photo"]}>
                   <IconComponent icon={IconImage} size={"15px"} />
