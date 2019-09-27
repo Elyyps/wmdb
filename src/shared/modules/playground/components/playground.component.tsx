92663d8904d1934ae40670d87612ca91215588d3
Ahmed-Mabrook <ahmed@webbio.nl>
2019-09-27 11:21:15 +0200

    fix: fixing design details

--- src/shared/modules/playground/components/playground.component.tsx
+++ src/shared/modules/playground/components/playground.component.tsx
@@ -19,12 +19,45 @@ import ROOMS from "@assets/icons/rooms.svg";
 import { CardPost } from "@app/prep/modules-prep/core";
 import { FooterComponent } from "@app/modules/footer";
 import { generateFooterData } from "@app/api/modules/footer/end-point";
+import { NavBarComponent } from "@app/modules/nav-bar";
+import { generateNavbarData } from "@app/api/modules/nav-bar/end-point";
+import { OutingCardComponent } from "@app/core/outing-card";
+import PlaceholderImage from "@assets/img-big.jpg";
+import PlaceholderImage1 from "@assets/img-center.jpg";
 export interface IPlaygroundComponentProps {}
 const imageList = [Oasis, Image, Oasis, Big, Grid, Rectangle];
+const card = {
+  title: "Escape City - Het ontsnappingsspel van Mokum",
+  subtitle: "Citygames in Amsterdam",
+  image: [PlaceholderImage, PlaceholderImage1, PlaceholderImage, PlaceholderImage1],
+  button: {
+    title: "Lees meer",
+    href: "#"
+  },
+  data: {
+    icon: ROOMS,
+    label: "10 - 1000 personen"
+  },
+  content:
+    "Amsterdamse Uitjes: Speel het bekende spel ‘Escape Room’ nu als uniek stadsspel: Escape City. De sport van het aangaan van uitdagingen en opdrachten, het oplossen van raadsels en het ontcijferen van codes…"
+};
+const navbarData = generateNavbarData();
+const footerData = generateFooterData();
 const PlaygroundComponent = (props: IPlaygroundComponentProps & PlaygroundContainerProps) => (
-  <div className={""}>
-    <FooterComponent footer={generateFooterData()} />
-  </div>
+  <React.Fragment>
+    {/* <div className={""}>
+      <NavBarComponent navbar={navbarData} />
+    </div> */}
+    <div className={""}>
+      <FooterComponent footer={footerData} />
+    </div>
+    {/* <div className={"uk-container"}>
+      <OutingCardComponent {...card} />
+    </div> */}
+    {/* <div className={"uk-container"}>
+      <HeaderGalleryComponent images={imageList} />
+    </div> */}
+  </React.Fragment>
 );
 
 export { PlaygroundComponent };
