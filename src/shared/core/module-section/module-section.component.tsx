44ff9d3854f3dc90469462800e1655879745248f
Ahmed-Mabrook <ahmed@webbio.nl>
2019-10-01 16:35:52 +0200

    feat: added background image to section module

--- src/shared/core/module-section/module-section.component.tsx
+++ src/shared/core/module-section/module-section.component.tsx
@@ -1,16 +1,19 @@
 import * as React from "react";
 import styles from "./module-section-component.module.scss";
 import classNames from "classnames";
+import { ImageComponent } from "../image";
 
 export interface IModuleSectionComponentProps {
   align?: string;
   backgroundColor?: string;
+  backgroundImage?: string;
   children?: any;
   paddingBottom?: string;
   paddingTop?: string;
   pattern?: boolean;
   size?: string;
   title?: string;
+  titleColor?: string;
 }
 
 const ModuleSectionComponent = (props: IModuleSectionComponentProps) => {
@@ -28,9 +31,20 @@ const ModuleSectionComponent = (props: IModuleSectionComponentProps) => {
 
   return (
     <div className={sectionClassName} style={style}>
-      <div className="uk-container">
-        {props.title && <h2 className={styles["module-section__title"]}>{props.title}</h2>}
-        <div>{props.children}</div>
+      {props.backgroundImage && (
+        <div className={styles["module-section__background"]}>
+          <ImageComponent src={props.backgroundImage} />
+        </div>
+      )}
+      <div className={styles["module-section__foreground"]}>
+        <div className="uk-container">
+          {props.title && (
+            <h2 className={styles["module-section__title"]} style={{ color: props.titleColor }}>
+              {props.title}
+            </h2>
+          )}
+          <div>{props.children}</div>
+        </div>
       </div>
     </div>
   );
