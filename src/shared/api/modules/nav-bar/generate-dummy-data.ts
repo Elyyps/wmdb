4c7fb18a87a156db9ca71103fc1c32f1c4991438
Ahmed-Mabrook <ahmed@webbio.nl>
2019-10-11 15:43:39 +0200

    feat: merging for sprint 2 demo

--- src/shared/api/modules/nav-bar/generate-dummy-data.ts
+++ src/shared/api/modules/nav-bar/generate-dummy-data.ts
@@ -9,5 +9,9 @@ export const generateDummyNavbarData = (): INavBarModule => ({
   ],
   logo: "",
   title: "Menu",
-  menuItems: [{ title: "Lorem1", url: "" }, { title: "Lorem2", url: "" }, { title: "Lorem3", url: "" }]
+  menuItems: [
+    { title: "Helpdesk", url: "/contact" },
+    { title: "Nieuwsbrief", url: "/info-page" },
+    { title: "Mijn uitjes", url: "/overzicht" }
+  ]
 });
