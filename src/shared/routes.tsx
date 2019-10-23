5b182418108c272d1b9f09e5d35da97527e9e297
Ahmed-Mabrook <ahmed@webbio.nl>
2019-10-23 11:01:31 +0200

    fix: fixed route for not found page

--- src/shared/routes.tsx
+++ src/shared/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { SingleWMDBPage } from "./pages/simple-wmdb/single-wmdb-page.component";
 import { FAQPageComponent } from "./pages/faq/faq-page";
 import { HomePageComponent } from "./pages/home/home-page";
@@ -8,12 +8,14 @@ import { NotFoundPageComponent } from "./pages/not-found/not-found-page";
 
 const Routes: React.FunctionComponent = props => (
   <React.Fragment>
-    <Route exact path="/" component={HomePageComponent} />
-    <Route exact path="/overzicht" component={OverviewPageComponent} />
-    <Route exact path="/single-wmdb" component={SingleWMDBPage} />
-    <Route exact path="/info-page" component={FAQPageComponent} />
-    <Route exact path="/not-found" component={NotFoundPageComponent} />
-    <Route path="*" component={NotFoundPageComponent} />
+    <Switch>
+      <Route exact path="/" component={HomePageComponent} />
+      <Route exact path="/overzicht" component={OverviewPageComponent} />
+      <Route exact path="/single-wmdb" component={SingleWMDBPage} />
+      <Route exact path="/info-page" component={FAQPageComponent} />
+      <Route exact path="/not-found" component={NotFoundPageComponent} />
+      <Route path="*" render={() => <Redirect to="/not-found" />} />
+    </Switch>
   </React.Fragment>
 );
 
